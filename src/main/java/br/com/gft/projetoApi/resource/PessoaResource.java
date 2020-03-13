c17356292a406875f285aa762d7e43417acd5266
Leandro de Lima Coelho <leandrofreestyle18@gmail.com>
2020-03-13 15:57:10 -0300

    Fazendo alteração

--- src/main/java/br/com/gft/projetoApi/resource/PessoaResource.java
+++ src/main/java/br/com/gft/projetoApi/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,16 @@ public class PessoaResource {
 		} else {
 			return ResponseEntity.noContent().build();
 		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		Pessoa pessoa = pessoaRepository.getOne(id);
+		
+		pessoaRepository.delete(pessoa);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
