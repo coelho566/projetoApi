12a6d60c01f3f0ada50267ceaa980ddfa6081a02
Leandro de Lima <leandrofreestyle18@gmail.com>
2020-04-01 09:18:29 -0300

    Implementando spring security

---
+++ bin/src/main/resources/db/migration/V03__criar_tabela_lancamento.sql
@@ -0,0 +1,14 @@
+CREATE TABLE lancamento (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(50) NOT NULL,
+	data_vencimento DATE NOT NULL,
+	data_pagamento DATE,
+	valor DECIMAL(10,2) NOT NULL,
+	observacao VARCHAR(100),
+	tipo VARCHAR(20) NOT NULL,
+	codigo_categoria BIGINT(20) NOT NULL,
+	codigo_pessoa BIGINT(20) NOT NULL,
+	FOREIGN KEY (codigo_categoria) REFERENCES categoria(id),
+	FOREIGN KEY (codigo_pessoa) REFERENCES pessoa(id)	
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
